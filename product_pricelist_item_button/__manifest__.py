a703eb591bfe1790b28c286da549a43c04c8f854
Luisa Miguéns (Solvos) <luisa.miguens@solvos.es>
2020-02-10 13:34:27 +0100

    [ADD] product_pricelist_item_button

---
+++ product_pricelist_item_button/__manifest__.py
@@ -0,0 +1,21 @@
+# © 2020 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Product pricelist item button",
+    "summary": """
+        Adds a button in the tariff to access to the Product pricelist 
+        with an independent view
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "12.0.1.0.0",
+    "category": "Product",
+    "website": "https://github.com/solvosci/slv-product-attribute",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_pricelist_views.xml",
+    ],
+    'installable': True,
+}
