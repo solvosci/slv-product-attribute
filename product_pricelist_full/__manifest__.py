acf7a74bce6986c9364cd3b131b0aa8053d79ac6
David Alonso (Solvos) <david.alonso@solvos.es>
2020-02-18 13:57:15 +0100

    [ADD] product_pricelist_full

---
+++ product_pricelist_full/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2020 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+{
+    "name": "Product Pricelist Full",
+    "summary": "Adds full support for pricelist edition from products",
+    "version": "12.0.1.0.0",
+    "category": "Product",
+    "website": "https://www.github.com/solvosci/slv-product-attribute",
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_pricelist_views.xml",
+        "views/product_template_views.xml",
+    ],
+}
