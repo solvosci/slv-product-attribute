f49a67121b3590847c3ceb0a45bdaadf1b453d58
David Alonso (Solvos) <david.alonso@solvos.es>
2020-03-03 16:59:58 +0100

    [IMP] product_pricelist_item_button: pricelist_id_invisible default value to True

--- product_pricelist_item_button/__manifest__.py
+++ product_pricelist_item_button/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.1.0",
     "category": "Product",
     "website": "https://github.com/solvosci/slv-product-attribute",
     "depends": [
